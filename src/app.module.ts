7d38d75208e036e0987e287c6fafe9b7c3dd7485
ayeolakenny <ayeolakenny@gmail.com>
2022-10-15 23:13:12 +0100

    configured knex for PROD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { KnexModule } from 'nest-knexjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
-import { devDatabaseConfig } from './config/db.config';
+import { devDatabaseConfig, prodDatabaseConfig } from './config/db.config';
 import { WalletModule } from './modules/wallet/wallet.module';
 
 @Module({
   imports: [
     KnexModule.forRoot({
-      config: devDatabaseConfig,
+      config:
+        process.env.NODE_ENV === 'production'
+          ? prodDatabaseConfig
+          : devDatabaseConfig,
     }),
     AuthModule,
     UserModule,
